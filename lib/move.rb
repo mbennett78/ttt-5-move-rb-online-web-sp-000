f92702133d32b0e2e56b8afa3fbb46f3ada64221
modern-helper-8600 <bennett.melissa1@gmail.com>
2020-02-07 03:35:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,8 @@ end
 # code your input_to_index and move method here!
 def input_to_index(user_input)
     user_input = user_input.to_i
-    return user_input -1
+    position = user_input -1
+    return position
 
 end
 
